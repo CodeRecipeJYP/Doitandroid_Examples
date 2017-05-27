7584363b7c65a484452acf9ea49d36837ccfeeff
pjo901018 <pjo901018@korea.ac.kr>
2017-05-28 00:06:51 +0900

    menu, main fragment오가도록 버튼 생성

--- app/src/main/java/com/asuscomm/yangyinetwork/chapter5/MainActivity.java
+++ app/src/main/java/com/asuscomm/yangyinetwork/chapter5/MainActivity.java
@@ -4,21 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//    MyMainFragment myMainFragment;
-//    MenuFragment menuFragment;
+    MyMainFragment myMainFragment;
+    MenuFragment menuFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        myMainFragment = (MyMainFragment)getSupportFragmentManager().findFragmentById(R.id.myMainFrag);
-//        menuFragment = new MenuFragment();
+        myMainFragment = (MyMainFragment) getSupportFragmentManager().findFragmentById(R.id.myMainFrag);
+        menuFragment = new MenuFragment();
     }
 
-//    public void onFragmentChanged(int index) {
-//        if(index == 0) {
-//            getSupportFragmentManager().beginTransaction().replace(R.id.container, menuFragment).commit();
-//        }
-//    }
+    public void onFragmentChanged(int index) {
+        if(index == 0) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, menuFragment).commit();
+        } else if(index == 1) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, myMainFragment).commit();
+        }
+    }
 }
--- app/src/main/java/com/asuscomm/yangyinetwork/chapter5/MenuFragment.java
+++ app/src/main/java/com/asuscomm/yangyinetwork/chapter5/MenuFragment.java
@@ -8,22 +8,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-/**
- * Created by jaeyoung on 2017. 5. 21..
- */
-
-public class MyMainFragment extends Fragment {
+public class MenuFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_main, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_menu, container, false);
 
         Button btn = (Button) rootView.findViewById(R.id.fragBtn);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 MainActivity activity = (MainActivity) getActivity();
-                activity.onFragmentChanged(0);
+                activity.onFragmentChanged(1);
             }
         });
 
