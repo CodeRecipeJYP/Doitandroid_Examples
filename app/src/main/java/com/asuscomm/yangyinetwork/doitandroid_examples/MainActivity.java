4e37464502996cd5558998291eb00f15c2d07369
pjo901018 <pjo901018@korea.ac.kr>
2017-05-28 01:11:04 +0900

    Async 완성

--- app/src/main/java/com/asuscomm/yangyinetwork/doitandroid_examples/MainActivity.java
+++ app/src/main/java/com/asuscomm/yangyinetwork/doitandroid_examples/MainActivity.java
@@ -1,13 +1,88 @@
 package com.asuscomm.yangyinetwork.doitandroid_examples;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    BackgroundTask task;
+
+    ProgressBar progress;
+    TextView progressTv;
+    int value;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progress = (ProgressBar) findViewById(R.id.progress);
+        progressTv = (TextView) findViewById(R.id.progressTv);
+
+        Button executeBtn = (Button) findViewById(R.id.executeBtn);
+        executeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                task = new BackgroundTask();
+                task.execute(100);
+            }
+        });
+
+        Button cancelBtn = (Button) findViewById(R.id.cancelBtn);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                task.cancel(true);
+            }
+        });
+    }
+
+    class BackgroundTask extends AsyncTask<Integer, Integer, Integer> {
+        @Override
+        protected void onPreExecute() {
+            value = 0;
+            progress.setProgress(value);
+        }
+
+        @Override
+        protected Integer doInBackground(Integer... params) {
+            while (isCancelled() == false) {
+                value++;
+                if (value >= 100) {
+                    break;
+                } else {
+                    publishProgress(value);
+                }
+
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return value;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            progress.setProgress(values[0].intValue());
+            progressTv.setText("Current Value : " + values[0].toString());
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            progress.setProgress(0);
+            progressTv.setText("Finished.");
+        }
+
+        @Override
+        protected void onCancelled() {
+            progress.setProgress(0);
+            progressTv.setText("Cancelled.");
+        }
     }
 }
