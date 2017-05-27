35e10acc72195662f0578fe53829b934f234e435
pjo901018 <pjo901018@korea.ac.kr>
2017-05-28 00:32:08 +0900

    ListFragment제작

--- app/src/main/java/com/asuscomm/yangyinetwork/chapter5/MainActivity.java
+++ app/src/main/java/com/asuscomm/yangyinetwork/chapter5/MainActivity.java
@@ -1,13 +1,27 @@
 package com.asuscomm.yangyinetwork.chapter5;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ListFragment.ImageSelectionCallback {
+    ListFragment listFragment;
+    ViewerFragment viewerFragment;
+
+    int[] images = {R.mipmap.abc, R.mipmap.ic_launcher, R.mipmap.ic_launcher_round};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager manager = getSupportFragmentManager();
+        listFragment = (ListFragment) manager.findFragmentById(R.id.listFragment);
+        viewerFragment = (ViewerFragment) manager.findFragmentById(R.id.viewerFragment);
+    }
+
+    @Override
+    public void onImageSelected(int position) {
+        viewerFragment.setImage(images[position]);
     }
 }
---
+++ app/src/main/java/com/asuscomm/yangyinetwork/chapter5/ViewerFragment.java
@@ -0,0 +1,31 @@
+package com.asuscomm.yangyinetwork.chapter5;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Created by jaeyoung on 2017. 5. 28..
+ */
+
+public class ViewerFragment extends Fragment {
+    ImageView imageView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_viewer, container, false);
+
+        imageView = (ImageView) rootView.findViewById(R.id.imageView);
+
+        return rootView;
+    }
+
+    public void setImage(int resId) {
+        imageView.setImageResource(resId);
+    }
+}
