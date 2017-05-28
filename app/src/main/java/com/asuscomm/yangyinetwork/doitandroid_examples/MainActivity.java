90648c570e9ed5cf3c3381532e491dd2c15d3dc3
pjo901018 <pjo901018@korea.ac.kr>
2017-05-28 12:00:09 +0900

    마지막 위치 경도위도 출력까지

--- app/src/main/java/com/asuscomm/yangyinetwork/doitandroid_examples/MainActivity.java
+++ app/src/main/java/com/asuscomm/yangyinetwork/doitandroid_examples/MainActivity.java
@@ -1,5 +1,11 @@
 package com.asuscomm.yangyinetwork.doitandroid_examples;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "onClick: btn1");
+                startLocationService();
             }
         });
 
@@ -43,7 +50,51 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void println(String s) {
-        tvStatus.setText(tvStatus.getText().toString()+ '\n'
-                +s);
+        tvStatus.setText(tvStatus.getText().toString() + '\n'
+                + s);
+    }
+
+
+    void requestPermissions(String permission){
+                int idx = 0;
+                if(ActivityCompat.shouldShowRequestPermissionRationale(
+                        this, permission)){
+                        Log.d(TAG, "requestPermissions: 권한설명 필요");
+                    } else {
+                        ActivityCompat.requestPermissions(
+                                        this, new String[] {permission}, idx);
+                    }
+            }
+
+    private void startLocationService() {
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(Manifest.permission.ACCESS_FINE_LOCATION);
+            requestPermissions(Manifest.permission.ACCESS_COARSE_LOCATION);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "startLocationService: permisision return");
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+        }
+
+        Log.d(TAG, "startLocationService: getLastLocation");
+        Location lastLocation = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (lastLocation != null) {
+
+            Double latitude = lastLocation.getLatitude();
+            Double longitude = lastLocation.getLongitude();
+            Log.d(TAG, "startLocationService: "+"Latitude : "+latitude +"Longitude : "+ longitude);
+            println("Latitude : "+latitude +"Longitude : "+ longitude);
+        }
+        else {
+            Log.d(TAG, "startLocationService: lastLocation is null");
+        }
     }
 }
