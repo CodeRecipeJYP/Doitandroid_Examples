8b47eebf64cd12856fc70a3108250877b24c1fe0
pjo901018 <pjo901018@korea.ac.kr>
2017-06-04 15:41:40 +0900

    chatroom_item_chat 생성 chatroom_actvity name변경

--- app/src/main/java/com/asuscomm/yangyinetwork/doitandroid_examples/ui/main/activities/ChatRoomActivity.java
+++ app/src/main/java/com/asuscomm/yangyinetwork/doitandroid_examples/ui/main/activities/ChatRoomActivity.java
@@ -1,11 +1,7 @@
 package com.asuscomm.yangyinetwork.doitandroid_examples.ui.main.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.asuscomm.yangyinetwork.doitandroid_examples.R;
 
@@ -14,7 +10,7 @@ public class ChatRoomActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat_room);
+        setContentView(R.layout.chatroom_activity);
     }
 
 }
