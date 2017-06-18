4d9638573412577b1ed785cb3c35ab2c9be028b0
pjo901018 <pjo901018@korea.ac.kr>
2017-06-18 14:20:08 +0900

    Sendbutton 추가

--- app/src/main/java/com/asuscomm/yangyinetwork/doitandroid_examples/ui/main/activities/ChatRoomActivity.java
+++ app/src/main/java/com/asuscomm/yangyinetwork/doitandroid_examples/ui/main/activities/ChatRoomActivity.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.asuscomm.yangyinetwork.doitandroid_examples.R;
 import com.asuscomm.yangyinetwork.doitandroid_examples.models.dummy.DummyData;
 import com.asuscomm.yangyinetwork.doitandroid_examples.ui.main.adapter.ChatlistRecyclerViewAdapter;
 
 public class ChatRoomActivity extends AppCompatActivity {
+    private final String TAG = "jaeyoung/"+getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,13 @@ public class ChatRoomActivity extends AppCompatActivity {
         chatlist.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false));
         ChatlistRecyclerViewAdapter adapter = new ChatlistRecyclerViewAdapter(DummyData.getChatsDummy());
         chatlist.setAdapter(adapter);
+        Button sendBtn = (Button) findViewById(R.id.chatroom_inputbar_btn_send);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: ");
+            }
+        });
     }
 
 }
